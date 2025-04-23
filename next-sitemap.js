b9875877650fc573e2165dd71bbd4dd6ecc8392c
Imad Johar <imadjohar4@gmail.com>
2025-04-23 17:21:53 +0100

    site map

---
+++ next-sitemap.js
@@ -0,0 +1,9 @@
+// next-sitemap.js
+module.exports = {
+    siteUrl: 'https://ironz.ma', // remplace par ton domaine réel
+    generateRobotsTxt: true,
+    sitemapSize: 5000,
+    changefreq: 'daily',
+    priority: 0.7,
+  };
+  
