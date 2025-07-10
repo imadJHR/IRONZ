bc22ad4a3e65d7c7eaf4cdf85ef731680ef61c4f
Imad Johar <imadjohar4@gmail.com>
2025-07-10 22:39:13 +0100

    testst

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ClientLayout from "./ClientLayout";
 
 export const metadata = {
-  title: "IRONZ PRO - Équipements Professionnels et Matériaux",
+  title: "IRONZ- Équipements Professionnels et Matériaux",
   description:
     "Votre partenaire de confiance pour tous vos besoins en équipements professionnels et matériaux de construction.",
   keywords:
