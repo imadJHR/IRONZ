c944e6dfaa703499b3dd68acd84e4eebfee35e62
unknown <muscleironz2019@gmail.com>
2025-09-10 22:26:02 +0200

    y

--- data/product.js
+++ data/product.js
@@ -167,6 +167,7 @@ import v5 from "../public/v5.jpg"
 import v6 from "../public/v6.jpg"
 import v7 from "../public/v7.jpg"
 import v8 from "../public/v8.jpg"
+import v9 from "../public/phototest.webp"
 
 export const categories = [
   {
@@ -3081,6 +3082,27 @@ export const products = [
     categoryId: "4",
     rating: 4.9,
     reviewCount: 89,
+    isNew: true,
+    isFeatured: true,
+    inStock: true,
+
+    relatedProducts: ["1", "17"],
+  },
+  {
+    id: "159",
+    name: "test",
+    description: "TEST notre ensemble de sudation haute performance, conçu pour vous faire transpirer comme jamais ! Grâce à sa doublure intérieure innovante qui piège la chaleur, ce survêtement augmente votre température corporelle pour accélérer la combustion des calories et la perte de poids en eau. Son design noir élégant et sa coupe confortable en font l'allié parfait pour la salle de sport, le running ou toute activité physique. Intensifiez chaque effort et atteignez vos objectifs plus rapidement !",
+    slug: "test",
+    price: 159,
+    taille: ["XL", "S"],
+    oldPrice: 200,
+    discount: 5,
+    image: v9,
+    gallery: [v9],
+    category: "Accessoires",
+    categoryId: "4",
+    rating: 4.9,
+    reviewCount: 70,
     isNew: true,
     isFeatured: true,
     inStock: true,
