bc3ef38ad3828e8ff77b528eaa5416445a2a7ec6
imad johar <imadjohar4@gmail.com>
2026-01-03 18:08:55 +0100

    r

--- api/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-async function connectDB(MONGO_URI) {
-  if (!MONGO_URI) throw new Error("MONGO_URI is missing");
-  await mongoose.connect(MONGO_URI);
-  console.log("MongoDB connected");
-}
-
-module.exports = connectDB;
