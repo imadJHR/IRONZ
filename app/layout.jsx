ff607ca9f83f8456f8f0929df97c13be980e2139
imad johar <imadjohar4@gmail.com>
2026-03-15 06:32:29 +0000

    vt

--- app/layout.jsx
+++ app/layout.jsx
@@ -70,6 +70,7 @@ export const metadata = {
     "instagram:creator": "@ironz_equipements",
     "instagram:site": "@ironz_official",
     "facebook-domain-verification": "bhglqyk56ty0ilu2xcfi7ok1pew1f2",
+     monetag: "4828e1edb553377b34517c12934f4fb1",
   },
 };
 
