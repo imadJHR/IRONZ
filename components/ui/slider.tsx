69180b09656cfb4046762dacc5f51d6cc40b38aa
imad johar <imadjohar4@gmail.com>
2026-04-22 01:44:59 +0100

    fezd

--- components/ui/slider.tsx
+++ components/ui/slider.tsx
@@ -4,30 +4,42 @@ import * as React from "react";
 import * as SliderPrimitive from "@radix-ui/react-slider";
 import { cn } from "../../lib/utils";
 
-const Slider = React.forwardRef(({ className, value = [], ...props }, ref) => {
-  const thumbs = React.useMemo(() => {
-    return Array.isArray(value) ? value.map((_, i) => (
-      <SliderPrimitive.Thumb
-        key={i}
-        className="block h-5 w-5 rounded-full border-2 border-yellow-500 dark:border-yellow-400 
-                   bg-white dark:bg-gray-900 ring-offset-white transition-colors 
-                   focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-yellow-400 
-                   focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
-      />
-    )) : null;
-  }, [value]);
+const Slider = React.forwardRef<
+  React.ElementRef<typeof SliderPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
+>(({ className, value, defaultValue, ...props }, ref) => {
+  // Determine the number of thumbs to render based on value or defaultValue
+  // Radix Slider 'value' is an array of numbers
+  const initialValue = Array.isArray(value) 
+    ? value 
+    : Array.isArray(defaultValue) 
+    ? defaultValue 
+    : [0];
 
   return (
     <SliderPrimitive.Root
       ref={ref}
-      className={cn("relative flex w-full touch-none select-none items-center", className)}
+      className={cn(
+        "relative flex w-full touch-none select-none items-center",
+        className
+      )}
       value={value}
+      defaultValue={defaultValue}
       {...props}
     >
       <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-gray-200 dark:bg-gray-700">
         <SliderPrimitive.Range className="absolute h-full bg-yellow-500 dark:bg-yellow-400" />
       </SliderPrimitive.Track>
-      {thumbs}
+      
+      {initialValue.map((_, i) => (
+        <SliderPrimitive.Thumb
+          key={i}
+          className="block h-5 w-5 rounded-full border-2 border-yellow-500 dark:border-yellow-400 
+                     bg-white dark:bg-gray-900 ring-offset-white transition-colors 
+                     focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-yellow-400 
+                     focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+        />
+      ))}
     </SliderPrimitive.Root>
   );
 });
